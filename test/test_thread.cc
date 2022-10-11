174194fc9727ab5787299917bc5dc077735cc4dd
zhaoyitian <460670327@qq.com>
2022-10-11 15:06:54 +0800

    semaphore and mutex

--- test/test_thread.cc
+++ test/test_thread.cc
@@ -3,12 +3,18 @@
 
 alotz::Logger::ptr g_logger = ALOTZ_LOG_ROOT();
 
+int count = 0;
+alotz::RWMutex s_mutex;
+
 void func1() {
     ALOTZ_LOG_INFO(g_logger) << "name: " << alotz::Thread::GetName()
                             << " this.name: " << alotz::Thread::GetThis()->GetName()
                             << " id: " << alotz::GetThreadId()
                             << " this.id: " << alotz::Thread::GetThis()->getId();
-    sleep(20);
+    for (int i = 0; i < 100000; ++i) {
+        alotz::RWMutex::WriteLock lock(s_mutex);
+        ++count;
+    }
 }
 
 void func2() {
@@ -28,6 +34,7 @@ int main(int argc, char** argv) {
     }
 
     ALOTZ_LOG_INFO(g_logger) << "thread test end.";
+    ALOTZ_LOG_INFO(g_logger) << "count = " << count;
 
     return 0;
 }
